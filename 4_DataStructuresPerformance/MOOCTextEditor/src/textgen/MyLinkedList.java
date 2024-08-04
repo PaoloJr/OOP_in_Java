da96f883290c8e761ae9219dc9f6f78c98c587cf
Paolo <pj.angeloni@gmail.com>
2024-08-03 20:27:20 -0400

    complete module 4

--- 4_DataStructuresPerformance/MOOCTextEditor/src/textgen/MyLinkedList.java
+++ 4_DataStructuresPerformance/MOOCTextEditor/src/textgen/MyLinkedList.java
@@ -17,24 +17,66 @@ public class MyLinkedList<E> extends AbstractList<E> {
 	/** Create a new empty LinkedList */
 	public MyLinkedList() {
 		// TODO: Implement this method
+		size = 0;
+		head = new LLNode<E>(null);
+		tail = new LLNode<E>(null);
+		head.next = tail;
+		tail.prev = head;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		LLNode<E> first = head;
+		
+		sb.append("\n[");
+		while (first != null) {
+			sb.append(first.data + ", ");
+			first = first.next;
+		}
+		sb.delete(sb.length() - 2, sb.length());
+		sb.append("]");
+		
+		return sb.toString();
 	}
 
 	/**
 	 * Appends an element to the end of the list
 	 * @param element The element to add
 	 */
-	public boolean add(E element ) 
-	{
+	public boolean add(E element) {
 		// TODO: Implement this method
-		return false;
+		
+		if (element == null) {
+			throw new NullPointerException("Element to add is null!");
+		}
+		
+		LLNode<E> newNode = new LLNode<E>(element);
+		newNode.prev = tail.prev;
+		newNode.next = tail;
+		tail.prev.next = newNode;
+		tail.prev = newNode;
+		
+		size++;
+		return true;
 	}
 
 	/** Get the element at position index 
 	 * @throws IndexOutOfBoundsException if the index is out of bounds. */
-	public E get(int index) 
-	{
+	public E get(int index) {
 		// TODO: Implement this method.
-		return null;
+		
+		if (index >= size|| index < 0) {
+			throw new IndexOutOfBoundsException("Index out of bounds to get!");
+		} else {
+			// currentNode is the one after the head (sentinel)
+			LLNode<E> currentNode = head.next;
+			// traverse the linked list by re-assigning currentNode until the index
+			for (int i = 0; i < index; i++) {
+				currentNode = currentNode.next;
+			}
+			return currentNode.data;
+		}
 	}
 
 	/**
@@ -42,17 +84,32 @@ public class MyLinkedList<E> extends AbstractList<E> {
 	 * @param The index where the element should be added
 	 * @param element The element to add
 	 */
-	public void add(int index, E element ) 
+	public void add(int index, E element) 
 	{
 		// TODO: Implement this method
+		if (index < 0 || index > size) {
+			throw new IndexOutOfBoundsException("Index out of bounds for add at index!");			
+		} else if (element == null) {
+			throw new NullPointerException("Element is null for add at index");
+		} else if (index == size) {
+			// if index is the same as the list size, call the add method to add it to the end
+			this.add(element);
+		} else {
+			LLNode<E> current = head;
+			for (int i = 0; i < index; i++) {
+				current = current.next;
+			}
+			new LLNode<E>(element, current, current.next);
+			size++;
+		}		
+		
 	}
 
 
 	/** Return the size of the list */
-	public int size() 
-	{
+	public int size() {
 		// TODO: Implement this method
-		return -1;
+		return this.size;
 	}
 
 	/** Remove a node at the specified index and return its data element.
@@ -64,7 +121,18 @@ public class MyLinkedList<E> extends AbstractList<E> {
 	public E remove(int index) 
 	{
 		// TODO: Implement this method
-		return null;
+		if (index >= size || index < 0) {
+			throw new IndexOutOfBoundsException("Index out of bounds to remove!");
+		} else {
+			LLNode<E> removeNode = head.next;
+			for (int i = 0; i < index; i++) {
+				removeNode = removeNode.next;
+			}
+			removeNode.next.prev = removeNode.prev;
+			removeNode.prev.next = removeNode.next;
+			size--;
+			return removeNode.data;
+		}
 	}
 
 	/**
@@ -77,12 +145,59 @@ public class MyLinkedList<E> extends AbstractList<E> {
 	public E set(int index, E element) 
 	{
 		// TODO: Implement this method
-		return null;
-	}   
+		if (index < 0 || index > size) {
+			throw new IndexOutOfBoundsException("Index out of bounds for set!");			
+		} else if (element == null) {
+			throw new NullPointerException("Element is null for set!");
+		} 
+		
+		LLNode<E> newNodeR = head.next;
+		for (int i = 0; i < index; i++) {
+			newNodeR = newNodeR.next;
+		}
+		E oldData = newNodeR.data;
+		newNodeR.data = element;
+		return oldData;
+	}
+	
+	public static void main(String args[]) {
+		MyLinkedList<Integer> ll = new MyLinkedList<Integer>();
+//		int indexGet = 2;
+//		int indexRemove = 0;
+//		
+//		ll.add(1);
+//		ll.add(2);
+//		ll.add(3);
+//		
+//		System.out.println("Linked List initial" + ll);
+//		System.out.println("Linked List initial size --> " + ll.size());
+//		System.out.println("Linked List get at index: " + indexGet + " --> " + ll.get(indexGet));
+//		System.out.println("Linked List remove at index: " + indexRemove + " --> " + ll.remove(indexRemove));
+//		System.out.println("Linked List after remove " + ll);
+//		System.out.println("Linked List size after remove " + ll.size());
+		
+		MyLinkedList<String> lls = new MyLinkedList<String>();
+		int indexGetString = 0;
+		int indexReplaceString = 0;
+		
+		lls.add("new");
+		lls.add("string");
+		lls.add("here");
+		
+		System.out.println("Linked List initial" + lls);
+		System.out.println("Linked List initial size --> " + lls.size());
+		lls.add(0, "another");
+		System.out.println("Linked List after add at index" + lls);
+		System.out.println("Linked List size after add at index " + lls.size());
+		System.out.println("Linked List get at index: " + indexGetString + " --> " + lls.get(indexReplaceString));
+		lls.set(indexReplaceString, "replaced");
+		System.out.println("Linked List after set" + lls);
+		
+	}
+	
 }
 
-class LLNode<E> 
-{
+class LLNode<E> {
 	LLNode<E> prev;
 	LLNode<E> next;
 	E data;
@@ -90,11 +205,19 @@ class LLNode<E>
 	// TODO: Add any other methods you think are useful here
 	// E.g. you might want to add another constructor
 
-	public LLNode(E e) 
-	{
-		this.data = e;
-		this.prev = null;
-		this.next = null;
+	public LLNode(E e) {
+		this.data = e;		
 	}
-
+	
+	public LLNode(E e, LLNode<E> prevNode, LLNode<E> nextNode) {
+		this(e);
+		this.next = nextNode;
+		this.prev = prevNode;
+		prevNode.next = this;
+		nextNode.prev = this;
+	}
+	
+	public String toString() {
+		return this.toString();
+	}	
 }
--- 4_DataStructuresPerformance/MOOCTextEditor/src/textgen/MyLinkedListTester.java
+++ 4_DataStructuresPerformance/MOOCTextEditor/src/textgen/MyLinkedListTester.java
@@ -16,7 +16,8 @@ import org.junit.Test;
  */
 public class MyLinkedListTester {
 
-	private static final int LONG_LIST_LENGTH =10; 
+	private static final int LONG_LIST_LENGTH = 20;
+	private static final int LIST1_LENGTH = 50;
 
 	MyLinkedList<String> shortList;
 	MyLinkedList<Integer> emptyList;
@@ -29,20 +30,22 @@ public class MyLinkedListTester {
 	@Before
 	public void setUp() throws Exception {
 		// Feel free to use these lists, or add your own
-	    shortList = new MyLinkedList<String>();
+		emptyList = new MyLinkedList<Integer>();
+
+		shortList = new MyLinkedList<String>();
 		shortList.add("A");
 		shortList.add("B");
-		emptyList = new MyLinkedList<Integer>();
+		shortList.add("C");
+		
 		longerList = new MyLinkedList<Integer>();
-		for (int i = 0; i < LONG_LIST_LENGTH; i++)
-		{
+		for (int i = 0; i < LONG_LIST_LENGTH; i++) {
 			longerList.add(i);
 		}
-		list1 = new MyLinkedList<Integer>();
-		list1.add(65);
-		list1.add(21);
-		list1.add(42);
 		
+		list1 = new MyLinkedList<Integer>();
+		for (int i = 0; i < LIST1_LENGTH; i++) {
+			list1.add(i);
+		}
 	}
 
 	
@@ -51,8 +54,7 @@ public class MyLinkedListTester {
 	/*You should not need to add much to this method.
 	 * We provide it as an example of a thorough test. */
 	@Test
-	public void testGet()
-	{
+	public void testGet() {
 		//test empty list, get should throw an exception
 		try {
 			emptyList.get(0);
@@ -64,6 +66,8 @@ public class MyLinkedListTester {
 		// test short list, first contents, then out of bounds
 		assertEquals("Check first", "A", shortList.get(0));
 		assertEquals("Check second", "B", shortList.get(1));
+		assertEquals("Check third", "C", shortList.get(2));
+		
 		
 		try {
 			shortList.get(-1);
@@ -73,7 +77,7 @@ public class MyLinkedListTester {
 		
 		}
 		try {
-			shortList.get(2);
+			shortList.get(3);
 			fail("Check out of bounds");
 		}
 		catch (IndexOutOfBoundsException e) {
@@ -106,56 +110,166 @@ public class MyLinkedListTester {
 	 * We've included the example from the concept challenge.
 	 * You will want to add more tests.  */
 	@Test
-	public void testRemove()
-	{
+	public void testRemove() {
 		int a = list1.remove(0);
-		assertEquals("Remove: check a is correct ", 65, a);
-		assertEquals("Remove: check element 0 is correct ", (Integer)21, list1.get(0));
-		assertEquals("Remove: check size is correct ", 2, list1.size());
+		assertEquals("Remove: check a is correct ", 0, a);
+		assertEquals("Remove: check element 0 is correct ", (Integer)1, list1.get(0));
+		assertEquals("Remove: check size is correct ", 49, list1.size());
 		
 		// TODO: Add more tests here
+		int b = list1.remove(1);
+		assertEquals("Remove: check element at index b ", 2, b);
+		assertEquals("Remove: check new element at 1", (Integer)3, list1.get(1));
+		assertEquals("Remove: check size is updated ", 48, list1.size());
+		
+		// lower bound
+		try {
+			list1.remove(-1);
+			fail("remove at -1 failed");
+		} 
+		catch (IndexOutOfBoundsException e) {
+			
+		}
+		// higher bound
+		try {
+			list1.remove(list1.size() + 1);
+			fail("remove at index > size");
+		}
+		catch (IndexOutOfBoundsException e) {
+			
+		}
 	}
 	
 	/** Test adding an element into the end of the list, specifically
 	 *  public boolean add(E element)
 	 * */
 	@Test
-	public void testAddEnd()
-	{
+	public void testAddEnd() {
         // TODO: implement this test
+		boolean first = shortList.add("D");
+		boolean second = shortList.add("E");
+		boolean third = shortList.add("F");
+		
+//		assertEquals("Add at end first: ", true, first);
+//		assertEquals("Add at end second: ", true, second);
+//		assertEquals("Add at end third: ", true, third);
+		assertTrue(first);
+		assertTrue(second);
+		assertTrue(third);
+		
+		// add null
+		try {
+			shortList.add(null);
+			fail("failed to add a null value");
+		}
+		catch (NullPointerException e) {
+			
+		}
 		
 	}
 
 	
 	/** Test the size of the list */
 	@Test
-	public void testSize()
-	{
+	public void testSize() {
 		// TODO: implement this test
-	}
-
-	
+		int emptyListSize = emptyList.size();
+		int shortListSize = shortList.size();
+		int longListSize = longerList.size();
+		int list1Size = list1.size();
+		
+		assertEquals("Test size; emptyList ", 0, emptyListSize);
+		assertEquals("Test size; shortList ", 3, shortListSize);
+		assertEquals("Test size; longerList ", 20, longListSize);
+		assertEquals("Test size; list1List ", 50, list1Size);
+		
+	}	
 	
 	/** Test adding an element into the list at a specified index,
 	 * specifically:
 	 * public void add(int index, E element)
 	 * */
 	@Test
-	public void testAddAtIndex()
-	{
+	public void testAddAtIndex() {
         // TODO: implement this test
+		// at at the end
+		shortList.add(3, "New Node");
+		shortList.add(4, "Another Node");
 		
+		// add at index
+		shortList.add(0, "addition Node");
+		shortList.add(1, "and another addition");
+		
+//		System.out.println(shortList);
+		
+		assertEquals("Short List add at index 0: ", "addition Node", shortList.get(0));
+		assertEquals("Short List add at index 1: ", "and another addition", shortList.get(1));
+		assertEquals("Short List add at index 5: ", "New Node", shortList.get(5));
+		assertEquals("Short List add at index 6: ", "Another Node", shortList.get(6));
+		
+		// lower bound
+		try {
+			shortList.add(-1, "new at -1");
+			fail("failed at index -1");
+		}
+		catch (IndexOutOfBoundsException e) {
+			
+		}
+		
+		// upper bound
+		try {
+			shortList.add(shortList.size() + 1, "new at size + 1");
+			fail("failed at index size + 1");
+		}
+		catch (IndexOutOfBoundsException e) {
+			
+		}
+		
+		// add null
+		try {
+			shortList.add(0, null);
+			fail("failed to add null-value at index 0");
+		}
+		catch (NullPointerException e) {
+			
+		}
 	}
 	
 	/** Test setting an element in the list */
 	@Test
-	public void testSet()
-	{
+	public void testSet() {
 	    // TODO: implement this test
-	    
+		shortList.set(0, "new replacement");
+		assertEquals("Short List set at index 0: ", "new replacement", shortList.get(0));
+		shortList.set(2, "index 2 replace");
+		assertEquals("Short List set at index 2: ", "index 2 replace", shortList.get(2));
+		
+		// lower bound
+		try {
+			shortList.set(-1, "some lower-bound index");
+			fail("set failed at -1");
+		}
+		catch (IndexOutOfBoundsException e) {
+			
+		}
+		
+		// higher bound
+		try {
+			shortList.set(shortList.size() + 1, "some higher-bound index");
+			fail("set failed at shortList size + 1");
+		}
+		catch (IndexOutOfBoundsException e) {
+			
+		}
+		
+		// set null value
+		try {
+			shortList.set(1, null);
+			fail("failed to set value at index 1 to null");
+		}
+	    catch (NullPointerException e) {
+	    	
+	    }
 	}
-	
-	
-	// TODO: Optionally add more test methods.
 	
 }
